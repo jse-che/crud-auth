ff2e4650d68a03c9fdc3a9538e18e5688bf69402
Jose Charris <jose.charris@horus-sc.com>
2024-05-07 12:37:03 -0500

    Register User

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () =>{
     try {
-        await mongoose.connect('mongodb://localhost/cruddb')
+        await mongoose.connect('mongodb+srv://jseche98:Jose101198.@crud.lklsbk5.mongodb.net/')
         console.log(">>> DB is Connected")
     } catch (error) {
         console.log(error)
